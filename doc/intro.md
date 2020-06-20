bda76cad451e502c2d0292f6dbf70af361bd5400
Cesar Alcancio <cesar.alcancio@payclip.com>
2020-06-20 15:09:53 -0500

    Genesis

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to project-loja-alura
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/project_loja_alura/core_test.clj
@@ -0,0 +1,7 @@
+(ns project-loja-alura.core-test
+  (:require [clojure.test :refer :all]
+            [project-loja-alura.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
